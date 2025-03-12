5bdb8166b95cd5f45fdab3ba04bec1eab488c176
Adarsh <adarshmaurya9415@gmail.com>
2025-03-12 08:50:47 +0000

    cors issue

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,14 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
-app.use(express.json())
+
 app.use(
   cors({
     origin: ['http://localhost:5173', 'http://192.168.110.180:5173', 'https://edu-gamix.vercel.app/'],
     credentials: true // Allows cookies/auth headers
   })
 )
+app.use(express.json())
 app.use('/user', userRoutes)
 
 const PORT = process.env.PORT || 3000
